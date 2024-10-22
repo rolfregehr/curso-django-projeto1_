f62995aaa7b0742a18505f520a3415572ce921ee
Rolf <rolf.regehr@gmail.com>
2024-10-22 20:58:40 -0300

    visualisação de uma receita configurada

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Rolf Regehr',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Luiz Otávio',
+    })
