53cac69c17082d7448870f188de9543792513f7f
Rolf <rolf.regehr@gmail.com>
2024-10-26 17:04:24 -0300

    ignorando o sqlite

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
+
+
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(20)],
+        'recipes': recipes,
     })
 
 
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
 def recipe(request, id):
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
