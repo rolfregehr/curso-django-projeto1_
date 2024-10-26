dc03c14711b2e8900c0959c8d05e5c65cf8df3b2
Rolf <rolf.regehr@gmail.com>
2024-10-26 16:57:04 -0300

    Filtros de categoria funcionandno

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
+
 
 
 class Recipe(models.Model):
@@ -26,3 +29,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
