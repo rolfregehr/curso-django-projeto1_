f3d0c029727e70460d014afd96743062352c5d87
Rolf <rolf.regehr@gmail.com>
2024-10-31 16:40:08 -0300

    testes corrigidos

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -34,3 +35,12 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+
+def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html')
