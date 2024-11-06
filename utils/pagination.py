0d3a2a11025dec9a7c6238ab9d69af81ddb35282
Rolf <rolf.regehr@gmail.com>
2024-11-06 12:58:13 -0300

    dotenv OPERANDO

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,6 +2,8 @@ import math
 
 from django.core.paginator import Paginator
 
+# Gera secret key
+# python -c import string as s;from random import SystemRandom as sr; print(''.join(sr().choices(s.ascii_letters s.punctuation, k=64)))
 
 def make_pagination_range(
     page_range,
